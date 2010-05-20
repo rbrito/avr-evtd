ea985a1b07676738ad87d54d4c1101541ae4fa40
Rogério Brito <rbrito@ime.usp.br>
2010-05-20 04:04:12 +0000

    Update copyright years.
    git-svn-id: file:///tmp/orig/avr-evtd/trunk@136 a2d00c94-3d26-0410-a74b-ce61611494ea

--- avr-evtd.c
+++ avr-evtd.c
@@ -1,25 +1,25 @@
 /*
-* Linkstation AVR daemon
-*
-* Copyright 2006 Bob Perry <lb-source@users.sf.net>
-* Copyright 2008, 2009 Rog�rio Brito <rbrito@users.sf.net>
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-* 02110-1301 USA.
-*
-*/
+ * Linkstation AVR daemon
+ *
+ * Copyright © 2006 Bob Perry <lb-source@users.sf.net>
+ * Copyright © 2008-2010 Rogério Brito <rbrito@users.sf.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ */
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
